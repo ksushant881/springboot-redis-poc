0045b90b807002dd7c4575df547c0766f50e030d
Sushant <71615295+ksushant881@users.noreply.github.com>
2024-11-10 22:17:04 +0530

    refactor: renaming and formatting files

--- src/main/java/com/example/spring/redis/model/Blog.java
+++ src/main/java/com/example/spring/redis/model/Blog.java
@@ -1,69 +1,66 @@
 package com.example.spring.redis.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.io.Serializable;
 
-import jakarta.persistence.*;
+@Entity @Table(name = "blogs") public class Blog implements Serializable {
 
-@Entity
-@Table(name = "blogs")
-public class Blog implements Serializable {
+    private static final long serialVersionUID = 1L;
 
-  private static final long serialVersionUID = 1L;
+    @Id @GeneratedValue(strategy = GenerationType.AUTO) private long id;
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private long id;
+    @Column(name = "title") private String title;
 
-  @Column(name = "title")
-  private String title;
+    @Column(name = "description") private String description;
 
-  @Column(name = "description")
-  private String description;
+    @Column(name = "published") private boolean published;
 
-  @Column(name = "published")
-  private boolean published;
+    public Blog() {
 
-  public Blog() {
+    }
 
-  }
+    public Blog(String title, String description, boolean published) {
+        this.title = title;
+        this.description = description;
+        this.published = published;
+    }
 
-  public Blog(String title, String description, boolean published) {
-    this.title = title;
-    this.description = description;
-    this.published = published;
-  }
+    public long getId() {
+        return id;
+    }
 
-  public long getId() {
-    return id;
-  }
+    public String getTitle() {
+        return title;
+    }
 
-  public String getTitle() {
-    return title;
-  }
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-  public void setTitle(String title) {
-    this.title = title;
-  }
+    public String getDescription() {
+        return description;
+    }
 
-  public String getDescription() {
-    return description;
-  }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-  public void setDescription(String description) {
-    this.description = description;
-  }
+    public boolean isPublished() {
+        return published;
+    }
 
-  public boolean isPublished() {
-    return published;
-  }
+    public void setPublished(boolean published) {
+        this.published = published;
+    }
 
-  public void setPublished(boolean published) {
-    this.published = published;
-  }
-
-  @Override
-  public String toString() {
-    return "Blog [id=" + id + ", title=" + title + ", desc=" + description + ", published=" + published + "]";
-  }
+    @Override
+    public String toString() {
+        return "Blog [id=" + id + ", title=" + title + ", desc=" + description + ", published=" + published + "]";
+    }
 
 }
